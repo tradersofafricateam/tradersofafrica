64973570661bca7f62c38f868bc2d99710feceb5
Wills <ejioguvictor6@gmail.com>
2022-10-17 12:07:03 +0100

    recent changes on the dashboard

--- src/pages/BuyerHub/pages/Dashboard/Dashboard.js
+++ src/pages/BuyerHub/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Iconly } from 'react-iconly'
+import React, { useState, useEffect, useMemo, useContext } from "react";
+import { Iconly } from "react-iconly";
 import Sidebar from "./components/Sidebar";
 import { Link } from "react-router-dom";
 
@@ -9,57 +9,148 @@ import ProductImgTable from "../../../../assets/img/products/p-img1.png";
 
 import "./Dashboard.css";
 
+import { GlobalContext } from "../../../../components/utils/GlobalState";
+import PaginationComponent from "./components/Pagination";
+import SearchInput from "./components/SearchInput";
+
 const Dashboard = () => {
+  const {
+    user,
+    userLoading,
+    userOrderSummary,
+    userEnquireSummary,
+    allUserOrder,
+    allUserEnquire,
+  } = useContext(GlobalContext);
+  console.log("all orders", allUserOrder);
+
+  //summary for all orders and enquire
+  const orderSummary =
+    userOrderSummary.total_delivered_orders +
+    userOrderSummary.total_pending_orders +
+    userOrderSummary.total_shipped_orders +
+    userOrderSummary.total_confirmed_orders;
+
+  const enquireSummary =
+    userEnquireSummary.total_pending_enquiries +
+    userEnquireSummary.total_received_quote +
+    userEnquireSummary.total_sent_enquiries;
+
+  const Capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  // Pagination
+  const [totalItems, setTotalItems] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const ITEMS_PER_PAGE = 10;
+
+  const ordersData = useMemo(() => {
+    let computedOrders = allUserOrder;
+
+    if (search) {
+      computedOrders = computedOrders.filter(
+        (order) =>
+          order.shippingType.toLowerCase().includes(search.toLowerCase()) ||
+          order.paymentTerm.toLowerCase().includes(search.toLowerCase()) ||
+          order.product.productName
+            .toLowerCase()
+            .includes(search.toLowerCase()) ||
+          order.status.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    setTotalItems(computedOrders.length);
+
+    return computedOrders.slice(
+      (currentPage - 1) * ITEMS_PER_PAGE,
+      (currentPage - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE
+    );
+  }, [allUserOrder, currentPage, search]);
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  if (userLoading) {
+    return (
+      <div
+        className="loader mx-auto"
+        align="center"
+        id="loader"
+        style={{
+          position: "absolute",
+          top: "calc(50% - 60px)",
+          left: "calc(50% - 60px)",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      ></div>
+    );
+  }
   return (
     <div>
-       <div className="grid-container">
+      <div className="grid-container">
         <div className="menu-icon">
           <i className="fas fa-bars header__menu"></i>
         </div>
-        
+
         <header className="header">
           <div className="header__message">
-            <h2>Hello Erhun!</h2>
+            <h2>{user.fullName ? Capitalize(user.fullName) : ""}!</h2>
           </div>
           <div className="header__search">
+            <SearchInput
+              onSearch={(value) => {
+                setSearch(value);
+                setCurrentPage(1);
+              }}
+              placeholder="Search for orders, order status and more"
+            />
 
-            <form>
-              <div className="custom__search">
-                <Iconly name='Search' set='light' primaryColor='#5C5C5C' size='medium' />
-                <input type="text" className="form-control custom-style" id="" placeholder="Search for orders, inquiries and more"/>
-              </div>
-            </form>
-            
             <div className="notify-wrap position-relative">
               <Link to="/dashboard">
-                <Iconly name='Notification' set='bulk' primaryColor='#282828' size='medium' />
+                <Iconly
+                  name="Notification"
+                  set="bulk"
+                  primaryColor="#282828"
+                  size="medium"
+                />
                 <span className="icon-notification position-absolute"></span>
               </Link>
             </div>
-
           </div>
         </header>
 
-       <Sidebar/>
+        <Sidebar />
 
         <main className="main">
-
-          {/* <div className="info-cards">
-            <div className="card">
-              <div>
-                <h2>Track it all!</h2>
-                <p>Keep track of all your and transactions activities here in your Dashboard</p>
+          {user.lastLoggedIn === null && (
+            <div className="info-cards">
+              <div className="card">
+                <div>
+                  <h2>Track it all!</h2>
+                  <p>
+                    Keep track of all your and transactions activities here in
+                    your Dashboard
+                  </p>
+                </div>
+                <img src={TrackImg} alt="..." />
               </div>
-              <img src={TrackImg} alt="..."/>
-            </div>
-            <div className="card">
-              <div>
-                <h2>Monitor Your Orders</h2>
-                <p>Keep track of all your and transactions activities here in your order tab on the dashboard</p>
+              <div className="card">
+                <div>
+                  <h2>Monitor Your Orders</h2>
+                  <p>
+                    Keep track of all your and transactions activities here in
+                    your order tab on the dashboard
+                  </p>
+                </div>
+                <img src={OrdersImg} alt="..." />
               </div>
-              <img src={OrdersImg} alt="..."/>
             </div>
-          </div>           */}
+          )}
 
           <h1 className="section-title">Activity Summary</h1>
           <div className="main-overview">
@@ -68,8 +159,10 @@ const Dashboard = () => {
                 <h2>Total Orders</h2>
                 {/* <p>Detailed transaction history is on the order page</p> */}
                 <div class="d-flex justify-content-between mt-4">
-                  <h3>125</h3>
-                  <Link className="overview-card-link" to="/orders">View all</Link>
+                  <h3>{orderSummary}</h3>
+                  <Link className="overview-card-link" to="/orders">
+                    View all
+                  </Link>
                 </div>
               </div>
             </div>
@@ -78,8 +171,10 @@ const Dashboard = () => {
                 <h2>Total Inquiries</h2>
                 {/* <p>Detailed transaction history is on the order page</p> */}
                 <div class="d-flex justify-content-between mt-4">
-                  <h3>22</h3>
-                  <Link className="overview-card-link" to="/inquiries">View all</Link>
+                  <h3>{enquireSummary}</h3>
+                  <Link className="overview-card-link" to="/inquiries">
+                    View all
+                  </Link>
                 </div>
               </div>
             </div>
@@ -109,101 +204,69 @@ const Dashboard = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>
-                        <div className="d-flex">
-                          <div className="flex-shrink-0">
-                            <img className="table-product-img" src={ProductImgTable} alt="..."/>
+                    {ordersData.map((orders, index) => (
+                      <tr key={index}>
+                        <td>
+                          <div className="d-flex">
+                            <div className="flex-shrink-0">
+                              <img
+                                className="table-product-img"
+                                src={orders.product.productImages[0].image}
+                                alt="product name"
+                              />
+                            </div>
+                            <div className="flex-grow-1 ms-3">
+                              <p>
+                                {orders.product.productName
+                                  ? Capitalize(orders.product.productName)
+                                  : ""}
+                              </p>
+                              <p className="table-order-no">
+                                Order {orders.orderNumber}
+                              </p>
+                            </div>
                           </div>
-                          <div className="flex-grow-1 ms-3">
-                            <p>Dried Hibiscus</p>
-                            <p className="table-order-no">Order No: 0123456543</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>USD 40,000</td>
-                      <td>FOB</td>
-                      <td>Letter of Credit</td>
-                      <td>
-                        <div className="text-warning">
-                          Pending
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div className="d-flex">
-                          <div className="flex-shrink-0">
-                            <img className="table-product-img" src={ProductImgTable} alt="..."/>
-                          </div>
-                          <div className="flex-grow-1 ms-3">
-                            <p>Dried Hibiscus</p>
-                            <p className="table-order-no">Order No: 0123456543</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>USD 40,000</td>
-                      <td>CIF</td>
-                      <td>Letter of Credit</td>
-                      <td>
-                        <div className="text-primary">
-                          Processing
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div className="d-flex">
-                          <div className="flex-shrink-0">
-                            <img className="table-product-img" src={ProductImgTable} alt="..."/>
-                          </div>
-                          <div className="flex-grow-1 ms-3">
-                            <p>Dried Hibiscus</p>
-                            <p className="table-order-no">Order No: 0123456543</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>XAF 20,000,000</td>
-                      <td>Local Delivery</td>
-                      <td>Letter of Credit</td>
-                      <td>
-                        <div className="text-success">
-                          Shipped
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div className="d-flex">
-                          <div className="flex-shrink-0">
-                            <img className="table-product-img" src={ProductImgTable} alt="..."/>
-                          </div>
-                          <div className="flex-grow-1 ms-3">
-                            <p>Dried Hibiscus</p>
-                            <p className="table-order-no">Order No: 0123456543</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>USD 40,000</td>
-                      <td>CFR</td>
-                      <td>Letter of Credit</td>
-                      <td>
-                        <div className="text-success">
-                          Delivered
-                        </div>
-                      </td>
-                    </tr>
+                        </td>
+                        <td>
+                          {orders.product.currency}{" "}
+                          {numberWithCommas(orders.cost)}
+                        </td>
+                        <td>{orders.shippingType}</td>
+                        <td>{orders.paymentTerm}</td>
+                        <td>
+                          {orders.status === "PENDING" && (
+                            <div className="text-warning ">Pending</div>
+                          )}
+                          {orders.status === "PROCESSING" && (
+                            <div className="text-primary ">Processing</div>
+                          )}
+                          {orders.status === "SHIPPED" && (
+                            <div className="text-info">Shipped</div>
+                          )}
+                          {orders.status === "DELIVERED" && (
+                            <div className="text-success">Delivery</div>
+                          )}
+                          {orders.status === "CANCELLED" && (
+                            <div className="text-secondary">Cancelled</div>
+                          )}
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
             </div>
           </div>
-
+          <PaginationComponent
+            total={totalItems}
+            itemsPerPage={ITEMS_PER_PAGE}
+            currentPage={currentPage}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
         </main>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
